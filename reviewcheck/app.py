424bc430ed2fb47e6c70f7ff2a8616eee631e0d7
Simon Bengtsson <simon.bengtsson.3@volvocars.com>
2022-05-23 13:32:18 +0200

    Create class for cli functionality
    Existing cli functionality is moved to the Cli class in cli.py

--- reviewcheck/app.py
+++ reviewcheck/app.py
@@ -8,16 +8,15 @@ Your configuration file should be in .config/reviewcheckrc and contain the follo
 
 Dependencies: rich
 """
-import argparse
 import datetime
 import json
 import logging
 import random
 import re
 import time
-from argparse import RawTextHelpFormatter
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
+from reviewcheck.cli import Cli
 
 import requests
 import yaml
@@ -149,60 +148,7 @@ colors = [
 
 
 def run() -> int:
-    parser = argparse.ArgumentParser(
-        description="""
-    This script will tell you which discussions on GitLab you need to respond to.
-    You will be shown the discussions from MRs where:
-
-    - You are the author
-    - Someone has replied to your comment but you haven't replied back
-    - Someone has referenced your @username
-
-    The script needs to be configured before you can use it. Add a ~/.config/reviewcheckrc
-    with the following content:
-
-    ```
-    secret_token: <get a secret token from your settings in gitlab>
-    user: <your username>
-    ```
-    """,
-        formatter_class=RawTextHelpFormatter,
-    )
-    parser.add_argument(
-        "-a",
-        "--all",
-        help="Show all threads, even when you don't need to reply",
-        action="store_true",
-        default=False,
-        dest="all",
-    )
-    parser.add_argument(
-        "-u",
-        "--user",
-        help="Username whose reviews you want to analyze",
-        action="store",
-        default=False,
-        dest="user",
-    )
-    parser.add_argument(
-        "-f",
-        "--fast-mode",
-        help="Enables fast mode, only checking MRs created in the last three weeks",
-        action="store_true",
-        default=False,
-        dest="fast",
-    )
-    parser.add_argument(
-        "-i",
-        "--ignore",
-        help="Space separated list of MRs to ignore, e.g. '-i 373 371'",
-        action="store",
-        nargs="+",
-        default=[],
-        dest="ignore",
-    )
-    args = parser.parse_args()
-
+    args = Cli.parse_arguments()
     config_path = Path.home() / ".config/reviewcheckrc"
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
